afc769a8ef04936dfc806129268e034859815391
ChanceToZoinks <thezoink@fakeemail.com>
2022-04-08 11:26:55 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Chessboardcoin game
+## Game rules
+Two people, A, B are in jail. The jailer is giving them a single chance to escape - they must beat the jailer at a game.
+The game rules are as follows:
+  - Person A will enter the room alone and the jailer will lay 64 coins on a chessboard, one on each square, in any configuration the jailer desires.
+  - The jailer will hide a single tiny key under one of the coins. The key is infinitely thin and it will not be possible to determine which coin the key is under based on any kind of physical disturbance caused by the key being under a coin.
+  - Person A can now flip exactly one coin on the board then must leave the room. Saying anything or doing anything other than thinking or flipping a coin while in the room is strictly prohibited (maybe some scratch paper and a pencil is allowed for calculations if needed). Any attempts to game the system, leave notes behind, or cheat in any way will result in instant death for both people.
+  - Person B will now enter the room and the same rules apply as to person A. Person B must now select the correct coin based solely on the information available to them on the board.
+  - If Person B correctly selects the coin the key is hidden under the jailer will release them both. The jailer will not cheat and will respect the rules of the game.
+  - Persons A and B will be given as much time as desired prior to the game to talk together and attempt to devise a strategy. However, the jailer will be fully aware of any strategy and can attempt to place the coins in such a way to attempt to defeat the strategy.
+
+
+## Playing it
+See the [notebook](notebook.ipynb) for an explanation of the strategy I came up with and explanation of some naming conventions.
+
+The playable version doesn't actually kill you if you guess wrong you can keep trying since its supposed to be a learning thing.
+### Playing single player:
+```
+git clone git@github.com:ChanceToZoinks/chessboardcoin.git
+cd chessboard_coin
+pip install -r requirements.txt
+python3 chessgame.py -s
+```
+You can try messing around with `strategy.yaml` if you want to change the bot behavior.
+
+### Playing with another person:
+```
+git clone git@github.com:ChanceToZoinks/chessboardcoin.git
+cd chessboard_coin
+pip install -r requirements.txt
+python3 chessgame.py
+```
+
+You could also maybe mess around in the notebook.
+
+### Commands
+|Command|Args|Description|
+|:------|:---|:-------------|
+|(q)uit || quit the game|
+|(g)uess |x y | guess the key is under (x, y)   x is left to right, y is top to bottom|
+|(b)oard || show the board|
+|(r)egion |region side | apply a mask to the board showing only the region/dual chosen|
+|(p)arity |region side | get the parity of the region identified by (region, side)|
+|(v)ector || get all 6 regions' paritys as a string e.g. 101100 where (0,0) is the leftmost slot and (5,0) is the rightmost|
+|(o)verlap |r0id r0s r1id r1s ... | apply a mask to the board showing only the intersection of an arbitrary number of regions identified by (rNid, rNs)|
+|(t)ell || tells you where the key is, multiplayer only|
+|(f)lip |x y | flip the coin at (x,y), multiplayer only|
+
+
+There's also some stuff in here like a book on coding theory and some notes/exercise solutions I left in here.
---
+++ chessgame.py
@@ -0,0 +1,317 @@
+#!/usr/bin/env python3
+import random
+from enum import Enum
+import itertools
+import shlex
+import readline
+from typing import Union
+import sys
+import yaml
+
+
+class Coin(Enum):
+    HEADS = 0
+    TAILS = 1
+
+    @staticmethod
+    def random_state():
+        return Coin[random.choice(Coin._member_names_)]
+
+
+class Square:
+    def __init__(self, x: int, y: int, coin: Coin) -> None:
+        self.x = x
+        self.y = y
+        self.num = 8 * y + x
+        self.coin = coin
+        self.is_key = False
+
+    def flip_coin(self) -> None:
+        self.coin = Coin(~self.coin.value % 2)
+
+    def __str__(self) -> str:
+        return f"{self.coin.value}"
+
+    def __repr__(self) -> str:
+        return f"Square(x={self.x}, y={self.y}, coin={self.coin.value}, num={self.num}, is_key={self.is_key})"
+
+
+class Board:
+    def __init__(self) -> None:
+        self.__generate_board()
+        self.__choose_key_square()
+        self.regions = self.__get_all_regions()
+
+    def __generate_board(self) -> None:
+        _ = []
+        for y, x in itertools.product(range(8), range(8)):
+            _.append(Square(x, y, Coin.random_state()))
+        self.board: list[Square] = _
+
+    def __choose_key_square(self) -> None:
+        _ = random.choice(self.board)
+        _.is_key = True
+
+    def __show_board(self, board) -> None:
+        for i, r in enumerate(board):
+            print(r, end=" ")
+            if i % 8 == 7:
+                print("\n", end="")
+
+    def __get_region(self, board_filter) -> list:
+        region = []
+        for square in self.board:
+            region.append(board_filter(square))
+        return region
+
+    def __board_region_mask(self, sq, region_id, side):
+        return sq if (sq.num // 2**region_id) % 2 == side else "-"
+
+    def __board_key_sq_mask(self, sq):
+        return "k" if sq.is_key else "-"
+
+    def __board_coords_mask(self, sq, x, y):
+        return sq if sq.x == x and sq.y == y else "-"
+
+    def __try_get_sq_by_coords(self, _x, _y, board=None):
+        _ = lambda x: x.x == _x and x.y == _y
+        if board:
+            sq = next(filter(_, board), None)
+        else:
+            sq = next(filter(_, self.board), None)
+        return sq
+
+    def __get_all_regions(self):
+        region_dict = {
+            0: {0: [], 1: []},
+            1: {0: [], 1: []},
+            2: {0: [], 1: []},
+            3: {0: [], 1: []},
+            4: {0: [], 1: []},
+            5: {0: [], 1: []},
+        }
+        for i in range(6):
+            _0 = lambda x: self.__board_region_mask(x, i, 0)
+            _1 = lambda x: self.__board_region_mask(x, i, 1)
+            region0 = self.__get_region(board_filter=_0)
+            region1 = self.__get_region(board_filter=_1)
+            region_dict[i][0] = region0
+            region_dict[i][1] = region1
+        return region_dict
+
+    def show_board(self) -> None:
+        self.__show_board(self.board)
+
+    def show_region(self, region_id: int, side: int) -> None:
+        if not isinstance(region_id, int) or not isinstance(side, int):
+            raise TypeError
+        if not side in (0, 1):
+            raise ValueError
+        if region_id > 5 or region_id < 0:
+            raise ValueError
+        region = self.regions[region_id][side]
+        print(f"region{region_id}, parity={self.get_region_parity(region)}")
+        self.__show_board(region)
+
+    def get_region_parity(self, region):
+        num_heads = 0
+        for elem in region:
+            if isinstance(elem, Square):
+                num_heads += 1 if elem.coin == Coin["HEADS"] else 0
+        return num_heads % 2
+
+    def show_region_parity(self, region_id: int, side) -> None:
+        if not isinstance(region_id, int) or not isinstance(side, int):
+            raise TypeError
+        if not side in (0, 1):
+            raise ValueError
+        if region_id > 5 or region_id < 0:
+            raise ValueError
+        print(
+            f"region{region_id} parity={self.get_region_parity(self.regions[region_id][side])}"
+        )
+
+    def show_region_parity_vectors(self) -> None:
+        l0 = []
+        l1 = []
+        for id, region in self.regions.items():
+            for side, squares in region.items():
+                if side == 0:
+                    n = self.get_region_parity(squares)
+                    l0.append(str(n))
+                if side == 1:
+                    n = self.get_region_parity(squares)
+                    l1.append(str(n))
+
+        print(f"side0: {''.join(l0)}\nside1: {''.join(l1)}")
+
+    def get_region_overlap(self, regions) -> list:
+        overlap = []
+        for x in list(zip(*regions)):
+            overlap.append(x[0]) if all(
+                map(lambda j: j == x[0], x)
+            ) else overlap.append("-")
+        return overlap
+
+    def show_region_overlap(self, region_params) -> None:
+        regions = []
+        for (id, side) in region_params:
+            regions.append(self.regions[id][side])
+        overlap = self.get_region_overlap(regions)
+        self.__show_board(overlap)
+
+    def check_if_square_in_region(
+        self, square: Square, region_id: int, side: int
+    ) -> bool:
+        return square in self.regions[region_id][side]
+
+    def check_if_coords_in_region(
+        self, x: int, y: int, region_id: int, side: int
+    ) -> bool:
+        sq = self.__try_get_sq_by_coords(x, y)
+        return self.check_if_square_in_region(sq, region_id, side)
+
+    def flip_coin(self, x: int, y: int) -> None:
+        sq = self.__try_get_sq_by_coords(x, y)
+        if sq:
+            sq.flip_coin()
+
+    def guess(self, x: int, y: int) -> bool:
+        sq = self.__try_get_sq_by_coords(x, y)
+        if sq:
+            return sq.is_key
+        return False
+
+    def get_key_square(self) -> Union[Square, None]:
+        for square in self.board:
+            if square.is_key:
+                return square
+
+    def show_only_key(self) -> None:
+        _ = lambda x: self.__board_key_sq_mask(x)
+        r = self.__get_region(board_filter=_)
+        self.__show_board(r)
+
+    def show_only_xy(self, x, y) -> None:
+        _ = lambda k: self.__board_coords_mask(k, x, y)
+        r = self.__get_region(board_filter=_)
+        self.__show_board(r)
+
+
+def load_strategy(strat_file: str = "strategy.yaml"):
+    with open(strat_file, "r") as f:
+        strat = yaml.load(f, Loader=yaml.Loader)
+    return strat
+
+
+def single_player_bot(B: Board):
+    print("Single player mode: Flipping for you.")
+    strategy = load_strategy()
+    sq = B.get_key_square()
+    regions = []
+    for region_id, strat in strategy["regions"].items():
+        r = B.regions[int(region_id)][strat["side"]]
+        r_dual = B.regions[int(region_id)][~strat["side"] % 2]
+        p = B.get_region_parity(r)
+        if B.check_if_square_in_region(sq, int(region_id), strat["side"]):  # type: ignore
+            if p != strat["parity"]:
+                regions.append(r)
+            else:
+                regions.append(r_dual)
+        else:  # in the dual
+            if p != ~strat["parity"] % 2:
+                regions.append(r)
+            else:
+                regions.append(r_dual)
+    overlap = B.get_region_overlap(regions)
+    sq_to_flip = [x for x in filter(lambda x: isinstance(x, Square), overlap)][0]
+    B.flip_coin(sq_to_flip.x, sq_to_flip.y)
+
+
+def play_game(import_arg=None):
+    single_player = False
+    try:
+        if sys.argv[1] in ("-s", "--single-player"):
+            single_player = True
+    except:
+        if import_arg in ("-s", "--single-player"):
+            single_player = True
+    B = Board()
+    if single_player:
+        single_player_bot(B)
+    while True:
+        try:
+            cmd, *args = shlex.split(input("What would you like to do?\n> ").lower())
+        except ValueError:
+            continue
+
+        if cmd == "q":
+            print(f"Square {repr(B.get_key_square())} was the key square.")
+            B.show_only_key()
+            break
+        elif cmd in ("g", "guess"):
+            try:
+                x, y = args
+                if B.guess(int(x), int(y)):
+                    print("Correct!")
+                    print(f"{repr(B.get_key_square())} was the key square.")
+                    B.show_only_key()
+                    break
+                else:
+                    print("Wrong. Guess again?")
+            except TypeError:
+                print("x and y must be integers")
+            except:
+                print("'(g)uess x y' to make a guess at position (x,y)")
+        elif cmd in ("b", "board", "show board"):
+            B.show_board()
+        elif cmd in ("p", "parity", "get region parity"):
+            try:
+                region_id, side = args
+                B.show_region_parity(int(region_id), int(side))
+            except TypeError:
+                print(
+                    "region_id must be an integer in [0, 5) and side an integer in [0,1]"
+                )
+            except:
+                print("'get region (p)arity region_id side' to get a region's parity")
+        elif cmd in ("r", "region", "show region"):
+            try:
+                region_id, side = args
+                B.show_region(int(region_id), int(side))
+            except TypeError:
+                print("region_id must be an integer in [0, 5)")
+            except ValueError:
+                print("region_id must be an integer in [0, 5)")
+            except:
+                print("'show (r)egion region_id side' to show a region")
+        elif cmd in ("f", "flip", "flip a coin") and not single_player:
+            try:
+                x, y = args
+                B.flip_coin(int(x), int(y))
+            except TypeError:
+                print("x and y must be integers")
+            except:
+                print("'(f)lip a coin x y' to flip the coin at position (x,y)")
+        elif cmd in ("t", "tell me", "key square") and not single_player:
+            print(f"{repr(B.get_key_square())} is the key square.")
+        elif cmd in ("v", "vector", "region num vector"):
+            B.show_region_parity_vectors()
+        elif cmd in ("o", "overlap", "show overlap"):
+            try:
+                _ = [int(e) for e in args]
+                B.show_region_overlap(list(zip(_[::2], _[1::2])))
+            except:
+                print(
+                    f"'show (o)verlap r0id r0s r1id r1s ...' to show region overlap. must use integers"
+                )
+        elif cmd in ("s", "show xy", "show only xy"):
+            try:
+                x, y = args
+                B.show_only_xy(int(x), int(y))
+            except:
+                print(f"'(s)how only xy x y', x and y must be integers")
+
+
+if __name__ == "__main__":
+    play_game()
